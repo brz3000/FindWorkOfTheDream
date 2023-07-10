a69910294f07cccc813f0adf655dbd98c7ff7f7c
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-07-11 01:16:17 +0300

    Update main.py
    Delete not needed functions with requests to site. Fix variables name

--- main.py
+++ main.py
@@ -5,20 +5,6 @@ from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
 
-def number_of_vacansies(vacancy=""):
-    url = 'https://api.hh.ru/vacancies'
-    params = {'professional_role': 96,
-              "area": 1,
-              "period": 31,
-              "text": f"программист {vacancy}"
-              }
-    headers = {"User-Agent": 'PostmanRuntime/7.30.1'}
-    response = requests.get(url, params=params, headers=headers)
-    response.raise_for_status()
-    vacansies = response.json()
-    return vacansies['found']
-
-
 def language_salary(vacancy="Python"):
     page = 0
     pages_number = 1
@@ -42,7 +28,7 @@ def language_salary(vacancy="Python"):
             page += 1
         except requests.exceptions.HTTPError as error:
             exit("Can't get data from server: \n{0}".format(error))
-    return salaries
+    return {'salaries': salaries, 'number_of_vacansies': page_payload['found']}
 
 
 def predict_rub_salary(salary):
@@ -94,29 +80,7 @@ def superjob_get_vacansies(vacancy=''):
         except requests.exceptions.HTTPError as error:
             exit("Can't get data from server: \n{0}".format(error))
         time.sleep(2)
-    return salaries
-
-
-def number_of_vacansies_sj(vacancy=""):
-    url = 'https://api.superjob.ru/2.0/vacancies/'
-    headers = {"User-Agent": 'PostmanRuntime/7.30.1',
-               "Host": "api.superjob.ru",
-               "X-Api-App-Id": os.environ['SJ_SECRET']}
-    params = {"login": os.environ['SJ_LOGIN'],
-              "password": os.environ['SJ_PASS'],
-              "client_id": os.environ['SJ_ID'],
-              "client_secret": os.environ['SJ_SECRET'],
-              "keyword": f"программист {vacancy}",
-              "town": 4
-              }
-    try:
-        response = requests.get(url, params=params, headers=headers)
-        response.raise_for_status()
-        payload = response.json()
-        count_vacansies_sj = payload['total']
-    except requests.exceptions.HTTPError as error:
-        exit("Can't get data from server: \n{0}".format(error))
-    return count_vacansies_sj
+    return {'salaries': salaries, 'number_of_vacansies': payload['total']}
 
 
 def predict_rub_salary_sj(salary):
@@ -151,16 +115,17 @@ def main():
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']
     ]
     for language in languages:
-        vacancies_found = number_of_vacansies(language)
-        predicts = [predict_rub_salary(salary) for salary in language_salary(language)]
+        vacancies_payload = language_salary(language)
+        vacancies_found = vacancies_payload['number_of_vacansies']
+        predicts = [predict_rub_salary(salary) for salary in language_salary(language)['salaries']]
         real_salaryes = [salary for salary in predicts if salary is not None]
         vacancies_processed = len(real_salaryes)
         if len(real_salaryes) != 0:
             average_salary = int(sum(real_salaryes) / len(real_salaryes))
         else:
             average_salary = None
-        average_salary_object = [language, vacancies_found, vacancies_processed, average_salary]
-        avg_salaries_hh.append(average_salary_object)
+        aggregate_one_language = [language, vacancies_found, vacancies_processed, average_salary]
+        avg_salaries_hh.append(aggregate_one_language)
         time.sleep(30)
     avg_salaries_hh_table = AsciiTable(avg_salaries_hh, title_hh)
 
@@ -169,15 +134,17 @@ def main():
     ]
 
     for language in languages:
-        predicts = [predict_rub_salary_sj(salary) for salary in superjob_get_vacansies(language)]
+        vacancies_payload = superjob_get_vacansies(language)
+        number_of_vacansies_sj = vacancies_payload['number_of_vacansies']
+        predicts = [predict_rub_salary_sj(salary) for salary in vacancies_payload['salaries']]
         real_salaryes = [salary for salary in predicts if salary is not None]
         vacancies_processed = len(real_salaryes)
         if len(real_salaryes) != 0:
             average_salary = int(sum(real_salaryes) / len(real_salaryes))
         else:
             average_salary = None
-        average_salary_object = [language, number_of_vacansies_sj(language), vacancies_processed, average_salary]
-        avg_salaries_sj.append(average_salary_object)
+        aggregate_one_language = [language, number_of_vacansies_sj, vacancies_processed, average_salary]
+        avg_salaries_sj.append(aggregate_one_language)
     avg_salaries_sj_table = AsciiTable(avg_salaries_sj, title_sj)
     return print(avg_salaries_hh_table.table), print(), print(avg_salaries_sj_table.table)
 
