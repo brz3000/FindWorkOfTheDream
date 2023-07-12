a2d5a6c54b4e23c33e4a2bc314d0ed8b803d0d1e
brz3000 <45814758+brz3000@users.noreply.github.com>
2023-07-12 14:58:09 +0300

    Update main.py
    Refactoring. Deleting nested "if/else" condition.  More cleaner and simple code.

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
 
-def get_salaries_hh(vacancy="Python"):
+ID_CITY_HH = 1
+ID_CITY_SJ = 4
+ID_PROFESSIONAL_ROLE_HH = 96
+COUNT_DAYS = 31
+VACANSIES_PER_PAGE = 100
+
+
+def get_salaries_hh(vacancy=""):
     page = 0
     pages_number = 1
     url = 'https://api.hh.ru/vacancies'
     headers = {"User-Agent": 'PostmanRuntime/7.30.1'}
     salaries = []
     while page < pages_number:
-        params = {"professional_role": 96,
-                  "area": 1,
-                  "period": 31,
+        params = {"professional_role": ID_PROFESSIONAL_ROLE_HH,
+                  "area": ID_CITY_HH,
+                  "period": COUNT_DAYS,
                   "text": f"программист {vacancy}",
-                  "per_page": 100,
+                  "per_page": VACANSIES_PER_PAGE,
                   "page": page}
         try:
             response = requests.get(url, params=params, headers=headers)
@@ -32,22 +39,17 @@ def get_salaries_hh(vacancy="Python"):
 
 
 def predict_rub_salary(salary):
-    if salary:
-        if salary['currency'] is None:
-            predict = None
-        else:
-            if salary['currency'] == 'RUR':
-                if salary['from'] is None:
-                    predict = salary['to'] * 0.8
-                elif salary['to'] is None:
-                    predict = salary['from'] * 1.2
-                else:
-                    predict = (salary['from'] + salary['to']) / 2
-            else:
-                predict = None
+    if salary is None or salary['currency'] is None or salary['currency'] != 'RUR':
+        return None
+    if salary['from'] is None and salary['to'] is None:
+        return None
+    if salary['to'] is None and salary['from'] is not None:
+        return salary['from'] * 1.2
+    if salary['to'] is not None and salary['from'] is None:
+        return salary['to'] * 0.8
     else:
-        predict = None
-    return predict
+        predict = (salary['from'] + salary['to']) / 2
+        return predict
 
 
 def get_salaries_superjob(vacancy=''):
@@ -64,9 +66,9 @@ def get_salaries_superjob(vacancy=''):
                   "client_id": os.environ['SJ_ID'],
                   "client_secret": os.environ['SJ_SECRET'],
                   "keyword": f"программист {vacancy}",
-                  "town": 4,
+                  "town": ID_CITY_SJ,
                   "page": page,
-                  "count": 100
+                  "count": VACANSIES_PER_PAGE
                   }
         try:
             response = requests.get(url, params=params, headers=headers)
@@ -84,23 +86,17 @@ def get_salaries_superjob(vacancy=''):
 
 
 def predict_rub_salary_sj(salary):
-    if salary[-1] is None:
-        predict = None
+    if salary[-1] is None or salary[-1] != "rub":
+        return None
+    if salary[0] == 0 and salary[1] == 0:
+        return None
+    if salary[0] > 0 and salary[1] == 0:
+        return salary[0] * 1.2
+    if salary[0] == 0 and salary[1] > 0:
+        return salary[1] * 0.8
     else:
-        if salary[-1] == 'rub':
-            if salary[0] == 0 & salary[1] == 0:
-                predict = None
-            elif salary[0] > 0 & salary[1] > 0:
-                predict = (salary[0] + salary[1]) / 2
-            elif salary[0] > 0 & salary[1] == 0:
-                predict = salary[0] * 1.2
-            elif salary[0] == 0 & salary[1] > 0:
-                predict = salary[1] * 0.8
-            else:
-                print("an incredible scenario")
-        else:
-            predict = None
-    return predict
+        predict = (salary[0] + salary[1]) / 2
+        return predict
 
 
 def main():
